959d96f718692aa8ecf804ce3a1350e3415c263f
Thanh Truong <thanh.truong@citigo.com.vn>
2017-04-21 06:27:40 +0700

    add signalr vs demo chathub

---
+++ TT.Dev/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TT.Dev.Startup))]
+namespace TT.Dev
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
